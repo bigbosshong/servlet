914de06311cf3138eff5dd72cd3d5c72bdd05e51
bigbosshong1101 <bigbosshong1101@gmail.com>
2023-12-07 00:16:39 +0900

    HTTP 응답 데이터 - API JSON

---
+++ src/main/java/hello/servlet/web/response/ResponseJsonServlet.java
@@ -0,0 +1,33 @@
+package hello.servlet.web.response;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servlet.basic.HelloData;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "responseJsonServlet", urlPatterns = "/response-json")
+public class ResponseJsonServlet extends HttpServlet {
+
+    protected ObjectMapper objectMapper = new ObjectMapper();
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //Content-type:application/json
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+
+        HelloData helloData = new HelloData();
+        helloData.setUsername("Hong");
+        helloData.setAge(31);
+
+        //{"username:"Hong", "age":31}
+        String result = objectMapper.writeValueAsString(helloData);
+        response.getWriter().write(result);
+
+
+    }
+}
