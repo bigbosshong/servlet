661b74a89ab9ac41779fc268c81c47cba6018266
bigbosshong1101 <bigbosshong1101@gmail.com>
2023-12-09 19:30:38 +0900

    필요없는 property 제거

--- src/main/java/hello/servlet/web/servlet/MemberFormServlet.java
+++ src/main/java/hello/servlet/web/servlet/MemberFormServlet.java
@@ -1,7 +1,6 @@
 package hello.servlet.web.servlet;
 
 
-import hello.servlet.domain.member.MemberRepository;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -13,7 +12,6 @@ import java.io.PrintWriter;
 
 @WebServlet(name = "memberFormServlet", urlPatterns = "/servlet/members/new-form")
 public class MemberFormServlet extends HttpServlet {
-    private MemberRepository memberRepository = MemberRepository.getInstance();
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
